620173923f69aacdb2543526f77f4993e387615f
evhalp <evhalp@gmail.com>
2026-03-12 15:16:31 -0400

    Add user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+
+    bio = models.TextField(blank=True, default='')
+    avatar_url = models.URLField(blank=True, default='')
+
+    class Meta:
+        db_table = 'users_user'
+
+    def __str__(self):
+        return self.username
