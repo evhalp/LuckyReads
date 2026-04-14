10e2497fd87e1d5b8c32713d9a04677c54ef33be
evhalp <evhalp@gmail.com>
2026-04-13 20:55:58 -0400

    Refactor review saved signal to use asynchronous task execution for recommendations

--- app/apps/books/signals.py
+++ app/apps/books/signals.py
@@ -3,13 +3,14 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from apps.books.models import Review
-from apps.recommendations.tasks import update_book_recommendations, update_buddy_recommendations
 
 @receiver(post_save, sender=Review)
 def on_review_saved(sender, instance, **kwargs):
-    user_id: int = instance.shelf_entry.user_id
     if not settings.DJANGO_ENABLE_CELERY:
         return
-    
-    update_book_recommendations(user_id)
-    update_buddy_recommendations(user_id)
+
+    from apps.recommendations.tasks import update_book_recommendations, update_buddy_recommendations
+
+    user_id: int = instance.shelf_entry.user_id
+    update_book_recommendations.delay(user_id)
+    update_buddy_recommendations.delay(user_id)
